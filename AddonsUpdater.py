79d470343e0071e21f6b0c9d02d0408aad02e281
Christopher Pedraza <gabalixreborn@gmail.com>
2024-01-30 16:14:40 -0600

    Add file copying and clean-up functionality

--- AddonsUpdater.py
+++ AddonsUpdater.py
@@ -1,5 +1,7 @@
 import configparser
 import requests
+import shutil
+import os
 
 # Initialize a config parser
 config = configparser.RawConfigParser()
@@ -54,45 +56,59 @@ def getReleaseAssets(source_data):
 
 def getCategoriesNames(categories, source_data):
     # Initialize a list to hold the category names
-    categories_names = []
+    # categories_names = []
+    categories_names = {}
     for value in source_data:
         for category in categories:
             # If the category is in the value, add the source data value to the category names
             if category in value:
-                categories_names.append(source_data[value])
+                categories_names[category] = source_data[value]
+                # categories_names.append(source_data[value])
     return categories_names
 
 
-def downloadReleaseAssets(assets, categories_names):
+def copyFile(fileName, destination):
+    shutil.copy2(f"downloads/{fileName}", destination)
+
+
+def downloadReleaseAssets(setup_values, assets, categories_names):
     for asset in assets:
-        # Initialize a flag to determine whether to download the asset
-        should_download = False
         for category in categories_names:
             # If the category is in the asset name, set the flag to download the asset
-            if category in asset["name"]:
-                should_download = True
-                break
-
-        if should_download:
-            # Print the name of the file being downloaded
-            print("Downloading '" + asset["name"] + "'", end=" .")
-            # Make a GET request to the asset download URL
-            file_dir = requests.get(asset["browser_download_url"])
-            print(".", end="")
-            # Write the content of the response to a file
-            open("downloads/" + asset["name"], "wb").write(file_dir.content)
-            print(". ", end="")
-            print("Finished.")
+            if categories_names[category] in asset["name"]:
+                # Print the name of the file being downloaded
+                print("Downloading '" + asset["name"] + "'", end=" .")
+                # Make a GET request to the asset download URL
+                file_dir = requests.get(asset["browser_download_url"])
+                print(".", end="")
+                # Write the content of the response to a file
+                open("downloads/" + asset["name"], "wb").write(file_dir.content)
+                print(". ", end="")
+                print("Finished.")
+                copyFile(asset["name"], setup_values[category + "_dir"])
 
 
-def getReleases(sources_values, categories):
+def getReleases(setup_values, sources_values, categories):
+    os.mkdir("downloads") if not os.path.exists("downloads") else None
     for source_data in sources_values:
         # Get the release assets for the source
         assets = getReleaseAssets(source_data)
         # Get the category names for the source
         categories_names = getCategoriesNames(categories, source_data)
         # Download the release assets
-        downloadReleaseAssets(assets, categories_names)
+        downloadReleaseAssets(setup_values, assets, categories_names)
+
+
+def cleanDownloads():
+    for filename in os.listdir("downloads"):
+        file_path = os.path.join("downloads", filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print("Failed to delete %s. Reason: %s" % (file_path, e))
 
 
 # Get the setup values from the config
@@ -102,4 +118,8 @@ categories = getCategories(setup_values)
 # Get the sources from the setup values
 sources_values = getSources(setup_values)
 # Get the releases for the sources
-getReleases(sources_values, categories)
+getReleases(setup_values, sources_values, categories)
+
+cleanDownloads() if input(
+    "Do you want to keep the downloaded files? (y/n) "
+) == "n" else None
